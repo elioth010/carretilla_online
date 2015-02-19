0e9d37fafde739740ac9e36f9d07be5d53e2f53f
Alexander Morales <klank4135@gmail.com>
2015-02-18 21:07:40 -0600

    Cambios en el script 1.sql, revisado y corregido

--- app/database/migrations/1.sql
+++ app/database/migrations/1.sql
@@ -1,4 +1,11 @@
-CREATE  TABLE users (
+/* CREATE DATABASE carretilla_online;
+*  CREATE USER 'carretilla'@'localhost' IDENTIFIED BY 'wrzJXmp2';
+*  GRANT ALL PRIVILEGES ON carretilla_online.* To 'carretilla'@'localhost';
+*/
+
+USE carretilla_online;
+
+CREATE  TABLE user (
   username VARCHAR(45) NOT NULL ,
   password VARCHAR(32) NOT NULL ,
   email VARCHAR(60) NOT NULL,
@@ -12,12 +19,12 @@ CREATE TABLE role(
   role_description VARCHAR(45) NOT NULL,
   PRIMARY KEY(id));
   
-CREATE TABLE user_roles (
+CREATE TABLE user_role (
   user_role_id INT(11) NOT NULL AUTO_INCREMENT,
   username VARCHAR(45) NOT NULL,
   role INT(3) NOT NULL,
   PRIMARY KEY (user_role_id),
   UNIQUE KEY uni_username_role (role,username),
   KEY fk_username_idx (username),
-  CONSTRAINT fk_user_role_username FOREIGN KEY (username) REFERENCES users (username),
+  CONSTRAINT fk_user_role_username FOREIGN KEY (username) REFERENCES user (username),
   CONSTRAINT fk_user_role_role FOREIGN KEY (role) REFERENCES role (id));
