d160ef60300f431ef8df3634bb1e85d315f7fb76
Alexander Morales <amorales@slechtvalk.elblogdeklank.net>
2015-02-18 20:49:24 -0600

    Anadidos modelos User, Role, UserRole y cambios en 1.sql

---
+++ app/models/UserRole.php
@@ -0,0 +1,19 @@
+<?php
+
+class UserRole extends Eloquent {
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'user_role';
+
+	/**
+	 * The attributes excluded from the model's JSON form.
+	 *
+	 * @var array
+	 */
+	//protected $hidden = array('password', 'remember_token');
+
+}
