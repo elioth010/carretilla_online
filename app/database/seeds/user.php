f8dff614e25f93ebfd8c5bff4fd866ef1dc6aa11
Alexander Morales <klank4135@gmail.com>
2015-04-10 17:21:43 +0000

    Adicion de seed para tabla user

---
+++ app/database/seeds/user.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+        array(
+                'username' => 'master',
+                'password' => Hash::make('online15'),
+				'email' => 'klank4135@gmail.com',
+				'name' => 'Alexander Morales',
+				'age' => 30,
+                'enabled' => 1
+        )
+);?>
