a70699abac662bc290ee8dc90d804cd0d9d515c9
Elioth Rivera <elioth010@elioth010.elioth.com>
2015-04-12 21:43:20 -0600

    agrego parte de la ruta ahora hay que migrar a sessionController

--- app/routes.php
+++ app/routes.php
@@ -1,19 +1,18 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
+  |--------------------------------------------------------------------------
+  | Application Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register all of the routes for an application.
+  | It's a breeze. Simply tell Laravel the URIs it should respond to
+  | and give it the Closure to execute when that URI is requested.
+  |
+ */
 
-Route::any('route', function()
-{
-	return View::make('hello');
+Route::any('route', function() {
+    return View::make('hello');
 });
 
 //Route::get('/', "HomeController@showWelcome");
@@ -23,50 +22,31 @@ Route::get('/users/update', "UserController@updateUser");
 Route::get('/users/delete', "UserController@deleteUser");
 
 //<<<<<<< HEAD
-Route::get('/', array('as' => 'home', function () { 
-    return View::make("home");
-}));
-
-Route::get('login', array('as' => 'login', function () {
-    
-}));
-
-Route::get('login', array('as' => 'login', function () {
-    return View::make('login');
-}))->before('guest');
+Route::get('/', array('as' => 'home', function () {
+        return View::make("home");
+    }));
 
 Route::post('login', function () {
     $user = array(
         'username' => Input::get('username'),
         'password' => Input::get('password')
     );
-    
+
     if (Auth::attempt($user)) {
-        return View::route('home')->with('flash_notice', 'You are successfully logged in.');
+        return Redirect::route('home')->with('flash_notice', 'You are successfully logged in.');
     }
+
     // authentication failure! lets go back to the login page
     return Redirect::route('login')->with('flash_error', 'Your username/password combination was incorrect.')->withInput();
 });
 
-Route::get('logout', array('as' => 'logout', function () { }));
+Route::get('logout', array('as' => 'logout', function () {
+        
+    }));
 
-Route::get('profile', array('as' => 'profile', function () { }));
-
-Route::filter('guest', function(){
-    if (Auth::check()){
-        return Redirect::route('home')->with('flash_notice', 'You are already logged in!');
-    }
-});
-
-Route::get('logout', array('as' => 'logout', function () { }))->before('auth');
-
-Route::get('profile', array('as' => 'profile', function () { }))->before('auth');
-
-Route::filter('auth', function(){
-        if (Auth::guest()){
-            return Redirect::route('login')->with('flash_error', 'You must be logged in to view this page!');
-        }
-});
+Route::get('profile', array('as' => 'profile', function () {
+        
+    }));
 
 Route::get('/roles', "RoleController@showRole");
 Route::get('/roles/add', "RoleController@addRole");
