9412f99136d3b252d6669c04aa5efea7f30d2e50
Alexander Morales <klank4135@gmail.com>
2015-04-29 02:48:56 +0000

    Primeros cambios para a;adir usuarios

--- app/views/users.blade.php
+++ app/views/users.blade.php
@@ -1,16 +1,43 @@
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-	<h1>Usuarios</h1>
-	<ul>
-	  @foreach($users as $usuario)
-	  <!-- Equivalente en Blade a <?php foreach ($users as $usuario) ?> -->
+@extends('layout')
+@section('content')
+<!-- check for login error flash var -->
+@if (Session::has('flash_error'))
+<div class="tooltip_box1" id="flash_error">{{ Session::get('flash_error') }}</div>
+@endif
 
-	     <li> {{ $usuario->username.' '.$usuario->name }} </li>
-	     <!-- Equivalente en Blade a <?php echo $usuario->username.' '.$usuario->name ?> -->
-	  @endforeach 
-	</ul>
-</body>
-</html>
+<section>
+<div id=principal class="container">
+	<div class="grid_1">
+	<table class="table table-striped">
+		<thead>
+			<tr>
+				<th>No.</th>
+				<th>Username</th>
+				<th>Email</th>
+				<th>Name</th>
+				<th>Age</th>
+				<th>Actions</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+	  		@foreach($users as $usuario)
+				<td> {{ $usuario->id }} </td>
+				<td> {{ $usuario->username }} </td>
+				<td> {{ $usuario->email }}</td>
+				<td> {{ $usuario->name }}</td>
+				<td> {{ $usuario->age }}</td>
+				<td> 
+					<a href="{{ action('UserController@updateUser', $usuario->id) }}" class="btn btn-default">Edit</a>
+					<a href="{{ action('UserController@deleteUser', $usuario->id) }}" class="btn btn-danger">Delete</a>
+				</td>
+			<br>
+			@endforeach
+			<a href="{{ action('UserController@addUser') }}" class="btn btn-success">Add User</a>
+			</tr>
+		</tbody>
+	</table>
+	</div>
+</div>
+</section> 
+@stop
